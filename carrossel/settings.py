53123dd9c4af12b01d52486523e3291d2141fbc8
DMB <douglasmbdev@gmail.com>
2025-06-27 14:50:44 -0300

    doing settings

--- carrossel/settings.py
+++ carrossel/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'carrossel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+# Production only
+# STATIC_ROOT= BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You are now logged in.')
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    else:
+        form = UserCreationForm()
+    
+    for field_name, field in form.fields.items():
+        field.widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': f'Enter your {field.label.lower()}'
+        })
+    
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
